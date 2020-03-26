a903b6ec82cf8f9d6fd96745e2083249eb054379
Paul Evans <decretist@gmail.com>
2020-03-25 18:48:03 -0700

    Add z-score function

--- burrows/burrows2.py
+++ burrows/burrows2.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 # Paul Evans (10evans@cua.edu)
-# 24 March 2020
+# 24-25 March 2020
 import statistics
 import sys
 sys.path.append('..')
@@ -26,17 +26,17 @@ def get_frequencies(features, subcorpora):
     the x authors’ subcorpora represented by this feature, as a
     percentage of the total number of words.
     '''
-    subcorpora_frequencies = {}
+    frequencies = {}
     empty = dict.fromkeys(features, 0)
     for subcorpus in subcorpora:
-        subcorpora_frequencies[subcorpus] = empty.copy()
+        frequencies[subcorpus] = empty.copy()
         subcorpus_tokens = u.tokenize('./corpus/' + subcorpus + '.txt')
         subcorpus_frequencies = u.frequencies(subcorpus_tokens)
         for feature in features:
-            subcorpora_frequencies[subcorpus][feature] = (subcorpus_frequencies.get(feature, 0) / len(subcorpus_tokens)) * 1000
-    return subcorpora_frequencies
+            frequencies[subcorpus][feature] = (subcorpus_frequencies.get(feature, 0) / len(subcorpus_tokens)) * 1000
+    return frequencies
 
-def get_statistics(frequencies):
+def get_means(frequencies):
     '''
     Then, calculate the mean and the standard deviation of these x
     values and use them as the offical mean and standard deviation
@@ -57,6 +57,23 @@ def get_statistics(frequencies):
         frequencies['stdevs'][feature] = statistics.stdev(frequency_list)
     return frequencies
 
+def get_z_scores(subcorpora, frequencies):
+    '''
+    For each of the n features and x subcorpora, calculate a z-score
+    describing how far away from the corpus norm the usage of this
+    particular feature in this particular subcorpus happens to be.
+    To do this, subtract the "mean of means" for the feature from
+    the feature’s frequency in the subcorpus and divide the result
+    by the feature’s standard deviation.
+    '''
+    z_scores = {}
+    features = list(frequencies[subcorpora[0]].keys())
+    for subcorpus in subcorpora:
+        z_scores[subcorpus] = dict.fromkeys(features, 0)
+        for feature in features:
+            z_scores[subcorpus][feature] = (frequencies[subcorpus][feature] - frequencies['means'][feature]) / frequencies['stdevs'][feature]
+    return z_scores
+
 def main(): # driver
     samples = ['cases', 'laws', 'marriage', 'other', 'penance', 'second']
     for sample in samples:
@@ -68,8 +85,10 @@ def main(): # driver
     sample_list.remove('cases')
     mfws = get_features(sample_list, 30)
     tmp = get_frequencies(mfws, sample_list)
-    tmp = get_statistics(tmp)
-    u.write_csv(tmp, './tmp.csv')
+    tmp = get_means(tmp)
+    u.write_csv(tmp, './f.csv')
+    tmp = get_z_scores(sample_list, tmp)
+    u.write_csv(tmp, './z.csv')
 
 if __name__ == '__main__':
     main()
